c97176b362f141ffd7c3f31cb5dd861e204a7ddb
Code_with_Manav <codewithmanav70@gmail.com>
2025-03-16 13:34:09 +0530

    Diamond Star Pattern

--- Control Structures/Pattern.py
+++ Control Structures/Pattern.py
@@ -1,26 +1,29 @@
-#Pattern like     
-#     A
-#    ABA
-#   ABCBA
-#  ABCDCBA
-# ABCDEDCBA
-
-def print_triangle_pattern(n):
+def print_diamond_pattern(n):
+    # Print the upper half of the diamond
     for i in range(n):
         # Print leading spaces
         for j in range(n - i - 1):
             print(" ", end="")
         
-        # Print the first half of the pattern
-        for j in range(i + 1):
-            print(chr(65 + j), end="")
+        # Print stars
+        for j in range(2 * i + 1):
+            print("*", end="")
         
-        # Print the second half of the pattern
-        for j in range(i - 1, -1, -1):
-            print(chr(65 + j), end="")
+        # Move to the next line
+        print()
+    
+    # Print the lower half of the diamond
+    for i in range(n - 2, -1, -1):
+        # Print leading spaces
+        for j in range(n - i - 1):
+            print(" ", end="")
+        
+        # Print stars
+        for j in range(2 * i + 1):
+            print("*", end="")
         
         # Move to the next line
         print()
 
 # Example usage
-print_triangle_pattern(5)
+print_diamond_pattern(int(input("Enter the number of rows for the diamond pattern: ")))
