6bbdaadce7dfc9ea850a94025e80c76d530fe052
Code_with_Manav <codewithmanav70@gmail.com>
2025-03-16 10:27:31 +0530

    Added Priority Order

--- Built In DataTypes/To_Do_List.py
+++ Built In DataTypes/To_Do_List.py
@@ -5,31 +5,31 @@ def display_tasks():
     if not task_list:
         print("No tasks in the list.")
     else:
-        for index, task in enumerate(task_list, start=1):
-            print(f"{index}. {task}")
+        for index, (task, priority) in enumerate(task_list, start=1):
+            print(f"{index}. {task} [{priority}]")
 
-def add_task(task):
-    task_list.append(task)
-    print(f"Task '{task}' added.")
+def add_task(task, priority):
+    task_list.append((task, priority))
+    print(f"Task '{task}' with priority '{priority}' added.")
 
 def remove_task(task_index):
     if 0 <= task_index < len(task_list):
-        removed_task = task_list.pop(task_index)
-        print(f"Task '{removed_task}' removed.")
+        removed_task, removed_priority = task_list.pop(task_index)
+        print(f"Task '{removed_task}' with priority '{removed_priority}' removed.")
     else:
         print("Invalid task index.")
 
-def update_task(task_index, new_task):
+def update_task(task_index, new_task, new_priority):
     if 0 <= task_index < len(task_list):
-        task_list[task_index] = new_task
-        print(f"Task {task_index + 1} updated to '{new_task}'.")
+        task_list[task_index] = (new_task, new_priority)
+        print(f"Task {task_index + 1} updated to '{new_task}' with priority '{new_priority}'.")
     else:
         print("Invalid task index.")
 
 def sort_tasks():
-    task_list.sort()
-    print("Tasks sorted.")
-
+    priority_order = {'Urgent': 1, 'Necessary': 2, 'Normal': 3}
+    task_list.sort(key=lambda x: priority_order[x[1]])
+    print("Tasks sorted by priority.")
 
 while True:
     print("\nTask Manager")
@@ -45,14 +45,22 @@ while True:
         display_tasks()
     elif choice == '2':
         task = input("Enter the task to add: ")
-        add_task(task)
+        priority = input("Enter the priority (Urgent, Necessary, Normal): ").capitalize()
+        if priority in ['Urgent', 'Necessary', 'Normal']:
+            add_task(task, priority)
+        else:
+            print("Invalid priority. Please enter one of Urgent, Necessary, Normal.")
     elif choice == '3':
         task_index = int(input("Enter the task index to remove: ")) - 1
         remove_task(task_index)
     elif choice == '4':
         task_index = int(input("Enter the task index to update: ")) - 1
         new_task = input("Enter the new task: ")
-        update_task(task_index, new_task)
+        new_priority = input("Enter the new priority (Urgent, Necessary, Normal): ").capitalize()
+        if new_priority in ['Urgent', 'Necessary', 'Normal']:
+            update_task(task_index, new_task, new_priority)
+        else:
+            print("Invalid priority. Please enter one of Urgent, Necessary, Normal.")
     elif choice == '5':
         sort_tasks()
     elif choice == '6':
